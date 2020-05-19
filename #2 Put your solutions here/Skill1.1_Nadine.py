a462a8c0effb0a44f05750d62eac15e3de1686b4
NadineSp <65215119+NadineSp@users.noreply.github.com>
2020-05-19 15:56:56 +0200

    .

---
+++ #2 Put your solutions here/Skill1.1_Nadine.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 14:45:30 2020
+
+@author: nadine.spormann
+"""
+
+
+num1 = input("Hello! Please tell me any number (int): ")
+num2 = input("Please tell me another number (int): ")
+num3 = input("Please tell me a third number (int): ")
+result = int(num1) + int(num2) + int(num3)
+
+print("Thank you!")
+print("Let me now sum up these numbers for you:")
+print(result)
