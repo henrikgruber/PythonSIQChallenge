f1b90a7c40a3e12e0f1f7b15952f8466ed65c7c2
Rieke1337 <65502959+Rieke1337@users.noreply.github.com>
2020-05-26 20:17:38 +0200

    Add files via upload

---
+++ #2 Put your solutions here/Skill2.4_Friederike.py
@@ -0,0 +1,13 @@
+# debug the below code so it works
+
+mystring = "hello"
+myfloat = 10.0
+myint = 20
+
+# testing code
+if mystring == "hello":
+    print("String: ", mystring)
+if isinstance(myfloat, float) and myfloat == 10.0:
+    print("Float: ", myfloat)
+if isinstance(myint, int) and myint == 20:
+    print("Integer: ", myint)
