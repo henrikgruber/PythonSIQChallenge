fcab5f846c1ab4fde8554079f3f3b27a036e3123
Tamina1234 <65281133+Tamina1234@users.noreply.github.com>
2020-06-26 15:41:25 +0200

    update gui farben

--- #3 TrackingGamificationApp/gui.py
+++ #3 TrackingGamificationApp/gui.py
@@ -8,14 +8,14 @@ def register_person():
     print("Registration started")
     user = username.get()
     pw = username.get()
-    print("REGISTRATION - Username: " + user + " Password:" + pw)
+    print("REGISTRATION - Username:" + user + " Password:" + pw)
   
 #Funktion nur für den Test der Gui angelegt - muss mit tatsächlicher Funktion ersetzt werden
 def login_person(): 
     print("Login started")
     user= username.get()
     pw = username.get()
-    print("LOGIN - Username: " + user + " Password:" + pw)
+    print("LOGIN - Username:" + user + " Password:" + pw)
 
 
 # Funktion für den Aufruf des Registrierungs Screens, die tatsächliche Registerierung erfolgt in einer andern Funktion 
@@ -24,6 +24,7 @@ def register():
     register_screen = Toplevel(main_screen)
     register_screen.title("Learn Tracking App Registration")
     register_screen.geometry('300x250')
+    register_screen.configure(bg='white')
     
     global username 
     global password 
@@ -35,20 +36,22 @@ def register():
     password = StringVar()
     
     #Eingabefelder/Buttons, muss immer den entsprechenden Screen hier angeben, sonst wird alles im Main Screen angezeigt 
-    Label (register_screen, text = " ").pack() #Abstandzeile
-    Label (register_screen, text = "Username:", font=('bold', 10)).pack()
-    username_entry = Entry (register_screen, textvariable = username).pack() 
-    Label (register_screen, text = "Password:", font=('bold', 10)).pack()
-    password_entry = Entry (register_screen, textvariable = password).pack()
-    Label (register_screen, text = " ").pack() #Abstandzeile
+    titel = Label (register_screen, text = "Registration", fg = "white", bg = 'steel blue', height = "2", width = "40", font='Opensans').pack()
+    Label (register_screen, text = " ", bg = 'white').pack() #Abstandzeile
+    Label (register_screen, text = "Username:", bg = 'white', font=('bold', 10)).pack()
+    username_entry = Entry (register_screen, bg = "ivory2", textvariable = username).pack() 
+    Label (register_screen, text = "Password:", bg = 'white', font=('bold', 10)).pack()
+    password_entry = Entry (register_screen, bg = "ivory2", textvariable = password).pack()
+    Label (register_screen, text = " ", bg = 'white').pack() #Abstandzeile
     #Aufrufen der tatsächlichen Registrierungs Funktion mit dem Button 
-    register_btn = Button (register_screen, text = "Register", command = register_person).pack()
+    register_btn = Button (register_screen, text = "Register", height = "1", width = "14", bg ='white', borderwidth = 1, command = register_person).pack()
    
 
 def login():
     login_screen = Toplevel(main_screen)
     login_screen.title("Learn Tracking App Login")
     login_screen.geometry('300x250')
+    login_screen.configure(bg='white')
     
     global username 
     global password 
@@ -60,27 +63,29 @@ def login():
     password = StringVar() #muss dann entsprechend der Passwort generierung hier umgeschrieben werden
     
     #Eingabefelder/Buttons, muss immer den entsprechenden Screen hier angeben, sonst wird alles im Main Screen angezeigt 
-    Label (login_screen, text = " ").pack() #Abstandzeile
-    Label (login_screen, text = "Username:", font=('bold', 10)).pack()
-    username_entry = Entry (login_screen, textvariable = username).pack() 
-    Label (login_screen, text = "Password:", font=('bold', 10)).pack()
-    password_entry = Entry (login_screen, textvariable = password).pack()
-    Label (login_screen, text = " ").pack() #Abstandzeile
-    login_btn = Button (login_screen, text = "Login", command = login_person).pack()
+    titel = Label (login_screen, text = "LOGIN", fg = "white", bg = 'steel blue', height = "2", width = "40", font='Opensans').pack()
+    Label (login_screen, text = " ", bg = 'white').pack() #Abstandzeile
+    Label (login_screen, text = "Username:", bg = 'white', font=('bold', 10)).pack()
+    username_entry = Entry (login_screen, bg = "ivory2", textvariable = username).pack() 
+    Label (login_screen, text = "Password:", bg = 'white', font=('bold', 10)).pack()
+    password_entry = Entry (login_screen, bg = "ivory2", textvariable = password).pack()
+    Label (login_screen, text = " ", bg = 'white').pack() #Abstandzeile
+    login_btn = Button (login_screen, text = "Login", height = "1", width = "14", bg ='white', borderwidth = 1, command = login_person).pack()
 
     
 def app_main():
-    #command = Zuordnen der Funktion zum Button
+    #command = Zuordnen der Funktion zum Button    
     global main_screen #um den screen von einer anderen funktion aufrufen zu können
     main_screen = Tk()
-    Label (text = " ").pack() #Abstandzeile
+    Label (text = " ", bg = 'white').pack() #Abstandzeile
     main_screen.title('Learn Tracking App') #Maskentitel
-    main_screen.geometry('300x250') #Maskengröße
-    titel = Label (text = "Learn Tracking App", font=('bold', 10)).pack() #Überschrift
-    Label (text = " ").pack() #Abstandzeile
-    login_btn = Button (text = "Login", height = "2", width = "30", command = login).pack()
-    Label (text = " ").pack() #Abstandzeile
-    register_btn = Button (text = "Register", height = "2", width = "30", command = register).pack()
+    main_screen.geometry('350x250') #Maskengröße
+    main_screen.configure(bg='white')
+    titel = Label (text = "Learn Tracking App", bg = 'white', font='Opensans').pack() #Überschrift
+    Label (text = " ", bg = 'white').pack() #Abstandzeile
+    login_btn = Button (text = "Login", height = "2", width = "20", bg = 'white', borderwidth = 1, command = login).pack()
+    Label (text = " ", bg = 'white').pack() #Abstandzeile
+    register_btn = Button (text = "Register", height = "2", width = "20", bg ='white', borderwidth = 1, command = register).pack()
     main_screen.mainloop()
 
 app_main()
